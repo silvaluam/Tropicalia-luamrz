9ed6447fde775f66cbb74524c506a19798140ea0
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-04-15 14:42:20 -0300

    15/04

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ document.addEventListener('DOMContentLoaded', function(){
 
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+
+    const alternaContraste = document.getElementById('alterna-contraste')
     
     let tamanhoAtualFonte = 1;
     
@@ -29,4 +31,11 @@ document.addEventListener('DOMContentLoaded', function(){
     tamanhoAtualFonte -= 0.1;
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
+
+    alternaContraste.addEventListener('click', function() {
+        document.body.classList.toggle('alto-contraste');
+    });
+
+
+
     });
