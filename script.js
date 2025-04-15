d4e78111272e46c150244f52569400359a3158eb
silvaluam <152218519+silvaluam@users.noreply.github.com>
2025-04-15 14:43:13 -0300

    15/04

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function(){
     document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
     });
 
-    alternaContraste.addEventListener('click', function() {
+    alternaContraste.addEventListener('click', function(){
         document.body.classList.toggle('alto-contraste');
     });
 
